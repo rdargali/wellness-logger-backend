ffbc061a4ea1ea7d577c3bb5d1e76605a08bc13d
Rawand Dargali <rdargali@yahoo.com>
2020-08-12 23:58:11 -0500

    schema

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+//mongodb
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+mongoose.connect("mongodb://localhost/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("MongoDB connected");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
